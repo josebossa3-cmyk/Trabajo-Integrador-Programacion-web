544e056a7ca2c15b60de8ba886085b5feb433838
emadupre <dupre.dev@gmail.com>
2025-10-22 17:07:54 -0300

    Actualización Proyecto

---
+++ src/script.js
@@ -0,0 +1,8 @@
+function menuHamburguesa() {
+  var x = document.getElementById("navbar");
+  if (x.className === "nav-bar") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav-bar";
+  }
+}
